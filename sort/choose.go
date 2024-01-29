1e75e612dde2faa7543dc0e243340a5fc1423052
Sparkmoons <82277295+Sparkmoons@users.noreply.github.com>
2024-01-29 16:22:53 +0300

    Update choose.go

--- sort/choose.go
+++ sort/choose.go
@@ -13,14 +13,13 @@ func ChooseSort(array []int) []int {
 	length := len(array)
 
 	for i := 0; i < length; i++ {
-  min := i
-
-  for j := i+1; j < length; j++ {
-			if array[j] < array[min] {
+		min := i
+		for j := i+1; j < length; j++ {
+		 if array[j] < array[min] {
 				min = j
 			}
 		}
-    array[i], array[min] = array[min], array[i]
+		array[i], array[min] = array[min], array[i]
 	}
 	return array
 }
