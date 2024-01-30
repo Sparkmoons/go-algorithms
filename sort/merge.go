649496e66dc19f5f3222df4abae8d7fda3e3964d
Sparkmoons <82277295+Sparkmoons@users.noreply.github.com>
2024-01-30 14:59:00 +0300

    Update merge.go

---
+++ sort/merge.go
@@ -0,0 +1,29 @@
+func MergeSort(array []int) []int {
+    if len(array) <= 1 {
+        return array
+    }
+    var left, right []int
+    mid := len(array) / 2
+    left = MergeSort(array[:mid])
+    right = MergeSort(array[mid:])
+    
+    return Merge(left, right)
+}
+
+func Merge(left, right []int) []int {
+    res := make([]int, 0, len(left) + len(right))
+    for len(left) > 0 || len(right) > 0 {
+        if len(left) == 0 {
+            res = append(res, right...)
+        } else if len(right) == 0 {
+            res = append(res, left...)
+        } else if left[0] < right[0] {
+            res = append(res, left[0])
+            left = left[1:]
+        } else {
+            res = append(res, right[0])
+            right = right[1:]
+        }
+    }
+    return res
+}
