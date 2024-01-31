f9c70390762bf8777c687732e143d8bb48697a1a
Sparkmoons <82277295+Sparkmoons@users.noreply.github.com>
2024-01-31 16:05:15 +0300

    Update linear.go

--- search/linear.go
+++ search/linear.go
@@ -1,3 +1,12 @@
+package sort
+
+import "fmt"
+
+func UseLinSearch(){
+    n := []int{1, 6, 4, 7, 10, 3, 5}
+    fmt.Println(LinSearch(n, 6))
+}
+
 func LinSearch(array []int, val int) int{
     for i, num := range array {
         if num == val {
