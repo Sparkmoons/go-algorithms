73633b7375d69cd7df788c591e3333f7e37ef04b
Sparkmoons <82277295+Sparkmoons@users.noreply.github.com>
2024-01-31 13:08:13 +0300

    Update binary.go

--- search/binary.go
+++ search/binary.go
@@ -1,1 +1,17 @@
-
+func BinSearch(array []int, val int) []int{
+    if len(array) == 0 {
+        return 0
+    }
+    first, right := 0, len(array) - 1
+    for first <= last {
+        mid := ((last - first) / 2) + first
+        if array[mid] == val {
+            return mis
+        } else if array[mid] > val {
+            last = mid - 1
+        } else if array[mid] < val {
+            first = first + 1
+        }
+    }
+    return 0
+}
